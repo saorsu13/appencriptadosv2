262d9d423d7d6b34e99d0035ecf48a3c5c301be3
saorsu13 <ortizsuarezsantiago13@gmail.com>
2025-05-11 00:43:41 -0500

    Cambios de estilos en homeScreen

--- src/styles/Home/HomeStyles.ts
+++ src/styles/Home/HomeStyles.ts
@@ -4,10 +4,8 @@ import { StyleSheet } from 'react-native';
 export const HomeStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
   },
   scrollContent: {
     padding: 16,
   },
-  // Aquí irán tus estilos de contenedores, márgenes, etc.
 });
