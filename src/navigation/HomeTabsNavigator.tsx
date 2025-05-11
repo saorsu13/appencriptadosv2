28244e3d6f957479f6c32d560eae0ed0d16c59bb
Santiago Ortiz <sortiz@ammper.com>
2025-05-11 00:53:13 -0500

    Correccion del header

--- src/navigation/HomeTabsNavigator.tsx
+++ src/navigation/HomeTabsNavigator.tsx
@@ -19,6 +19,7 @@ export default function HomeTabsNavigator() {
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: colors.backgroundSecondary },
+        headerShown: false,
         headerTintColor: colors.primaryText,
       }}
     >
